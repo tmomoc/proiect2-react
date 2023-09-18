5c55d175aafc1d411076a6d33ff34369f977a25b
Teo <teodoramomoc9@gmail.com>
2023-09-18 16:34:58 +0300

    modify App.js to be a class

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React from 'react';
+
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={};
+  }
+
+  render(){
+    return(
+      <div>
+          curs5
+      </div>
+
+    );
+  }
+}
+
+export default App;
